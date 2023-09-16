9bc0b1fc71ab45c2c8a4eccd9cf089840fe4c4f7
Lee816 <lgha0816@naver.com>
2023-09-16 10:52:32 +0900

    book_detail 에 로그인 안되있으면 에러 발생 해결

--- books/views.py
+++ books/views.py
@@ -41,7 +41,8 @@ class BookDetail(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(book=kwargs['object'])
         context['reviewform'] = ReviewForm()
-        context['can_review'] = Books_rental.objects.filter(book=kwargs['object'],user=self.request.user)
+        if self.request.user.is_authenticated:
+            context['can_review'] = Books_rental.objects.filter(book=kwargs['object'],user=self.request.user)
         return context
 
 class BookCreate(LoginRequiredMixin, PermissionRequiredMixin, generic.CreateView):
